9aed2c52d9795d318507a6a3eab9ec88d900e863
SaranyaGanesan90 <100024216+SaranyaGanesan90@users.noreply.github.com>
2022-02-24 20:10:46 +0530

    Add files via upload

---
+++ convertNegativeToPositive.java
@@ -0,0 +1,14 @@
+package assignment.week1;
+
+public class convertNegativeToPositive {
+
+	public static void main(String[] args) {
+		int n1=-40,n2;
+		if(n1<0) {
+			n2=n1*(-1);
+			System.out.println(n2);
+		}
+
+	}
+
+}
